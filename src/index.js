2bbade2d486c51cd2ca62c44cf6e903c04572b0b
chibabe97 <chidinmaojukwu321@gmail.com>
2024-10-06 16:10:54 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img src={img} />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
